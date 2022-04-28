745931c891610945fce12cdb22d4bd0ac20073a0
luke-oribine <85437350+luke-oribine@users.noreply.github.com>
2022-04-28 08:17:56 -0400

    Added crispy forms.

--- The_Travelling_Therapist/settings.py
+++ The_Travelling_Therapist/settings.py
@@ -39,8 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'auction',
-    'localflavor'
+    'localflavor',
+    'crispy_forms'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- auction/forms.py
+++ auction/forms.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 class AuctionForm(forms.ModelForm):
     class Meta:
         model = Auction
-        fields = ('auctionStart', 'auctionEnd', 'reservePrice', 'placementStart', 'placementEnd', 'payFrequency')
+        fields = ('auctionStart', 'auctionEnd', 'reservePrice', 'placementStart', 'placementEnd', 'payFrequency', 'mondayStart', 'mondayEnd')
         labels = {}
         widgets = {
             'auctionStart': forms.DateTimeInput(format=('%Y-%m-%d'), attrs={'class': 'form-control', 'placeholder': 'Select a date', 'type': 'date'}),
@@ -13,6 +13,8 @@ class AuctionForm(forms.ModelForm):
             'reservePrice': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Reserve Price'}),
             'placementStart': forms.DateInput(format=('%Y-%m-%d'), attrs={'class': 'form-control', 'placeholder': 'Select a date', 'type': 'date'}),
             'placementEnd': forms.DateInput(format=('%Y-%m-%d'), attrs={'class': 'form-control', 'placeholder': 'Select a date', 'type': 'date'}),
+            'mondayStart': forms.TimeInput(attrs={'class': 'form-control', 'placeholder': 'Select a date', 'type': 'time'}),
+            'mondayEnd': forms.TimeInput(attrs={'class': 'form-control', 'placeholder': 'Select a date', 'type': 'time'}),
         }
 
 class BidForm(forms.ModelForm):
