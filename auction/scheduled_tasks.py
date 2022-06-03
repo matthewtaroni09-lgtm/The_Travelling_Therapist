9be843e22b07c397e84eb2b539cc5efc51baedfa
traveling-therapist <85437350+luke-oribine@users.noreply.github.com>
2022-06-03 05:54:08 -0400

    Added end of auction emails.

--- auction/scheduled_tasks.py
+++ auction/scheduled_tasks.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime
 from django.core.mail import send_mail
@@ -42,9 +43,46 @@ def update_something(id):
         auction.save()
         if admin.sendEmails:
             print('send email')
+            # Therapist email
             send_mail(
                     subject = "Auction Ended",
-                    message = "<h1>Your auction ended at:</h1> " + dt_string + ". The winning bid was: " + str(winningBid.amount) + ".",
+                    message = "Your auction ended at: " + dt_string + ". The winning bid was: " + str(winningBid.amount) + ".",
+                    # html_message = "<h1>Your auction ended at:</h1> " + dt_string + ". The winning bid was: " + str(winningBid.amount) + ".",
+                    html_message = """<section style="font-size: 16px; margin-bottom: 4rem;">
+                            <p>Hello <span class="text-weight-bold">""" + auction.clinic.city + """</span>,</p>
+                            
+                            <p>You were the lowest bidder at the end of an auction using The Traveling Therapist. Congratulations!</p>
+                            <p>Look for an email from us shortly to match you with the clinic. From that point it is your responsibility to contact the clinic and arrange some of the details of your job like how you will be paid (cheque, deposit, etc.)</p>
+                            
+                            <p>Thank you and we look forward to having you use our service again soon,</p>
+                            <p style="font-weight: 700; padding-top: 0rem; margin-top: 0; line-height: 0;">The Traveling Therapist Team</p>
+
+                            </section>""",
+                    from_email = settings.EMAIL_HOST_USER,
+                    recipient_list = [winningBid.user.email]
+                )
+            # Clinic email
+            send_mail(
+                    subject = "Auction Ended",
+                    message = "Your auction ended at: " + dt_string + ". The winning bid was: " + str(winningBid.amount) + ".",
+                    # html_message = "<h1>Your auction ended at:</h1> " + dt_string + ". The winning bid was: " + str(winningBid.amount) + ".",
+                    html_message = """<section style="font-size: 16px; margin-bottom: 4rem;">
+                    <p>Hello <span class="text-weight-bold">""" + auction.clinic.clinicName + """</span>,</p>
+                    
+                    <p>You auction has ended and you will be matched with the lowest bidding therapist soon. Look for an email from us shortly to connect you.</p>
+
+                    <p>From this point, it is your responsibility to finalize the following outside of The Traveling Therapist with your temporary therapist:</p>
+                    <ul>
+                        <li>How they will be paid (cheque, transfer, etc.)</li>
+                        <li>What you need from them before starting. This could be a copy of their insurance, etc.</li>
+                        <li>Any site/clinic orientation required on day 1 or in advance of the work term.</li>
+                    </ul>
+
+                    <p>Your card on file will be charged by The Traveling Therapist within the next 24h.</p>
+
+                    <p>Thanks for using our service - we hope to see you again soon,</p>
+                    <p style="font-weight: 700; padding-top: 0rem; margin-top: 0; line-height: 0;">The Traveling Therapist Team</p>
+                    </section>""",
                     from_email = settings.EMAIL_HOST_USER,
                     recipient_list = [winningBid.user.email]
                 )
