be3deec58c97e81deba8c66c343a6f7354249447
Nick <nicholas@wealthyplanet.com>
2022-06-08 13:29:37 -0400

    Make current bid details one one line

--- auction/models.py
+++ auction/models.py
@@ -104,7 +104,7 @@ class Auction(models.Model):
         elif self.closed == True and self.active == False:
             return 'Winning bid: $' + str(self.winningPrice)
         else:
-            return 'Current Low Bid: $' + str(self.currentLowBid)
+            return 'Current Bid: $' + str(self.currentLowBid)
 
     def get_num_bids(self):
         num_bids = Bid.objects.filter(auction=self.auctionID).count()
