2a181380c2b6d0be009c3b4b014b359a1aeaac6e
traveling-therapist <85437350+luke-oribine@users.noreply.github.com>
2022-06-30 06:24:34 -0400

    Update to Cookie Policy for Google Analytics.

--- auction/apps.py
+++ auction/apps.py
@@ -3,7 +3,6 @@ from django.apps import AppConfig
 class AuctionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auction'
-    print('inside auction config')
     def ready(self):
         import auction.signals
 
