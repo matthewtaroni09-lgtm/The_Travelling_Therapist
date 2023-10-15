8d4b4fe1b08d81f6f86532a061628d6ae533ff1f
Luke Oribine <lukeoribine@Lukes-MacBook-Air.local>
2023-10-15 07:08:47 -0400

    Add payment type validations.

--- auction/static/auction/create_auction.js
+++ auction/static/auction/create_auction.js
@@ -126,6 +126,10 @@ $("#submitButton").click(function () {
         errorList += "<li>Please select an auction type.</li>";
     }
 
+    if ($("#id_paymentType").val() === "") {
+        errorList += "<li>Please select a payment type.</li>";
+    }
+
     let placementStart = new Date($("#id_placementStart").val());
     let placementEnd = new Date($("#id_placementEnd").val());
 
@@ -260,49 +264,53 @@ $("#submitButton").click(function () {
         }
     }
     
-    //Treatment Cost
-    if(treatmentCost === ""){
-        errorList += "<li>Please enter a Treatment Cost.</li>";
-    }
-    if(treatmentCost > costMax && treatmentCost !== ""){
-        errorList += "<li>Treatment costs cannot exceed " + currencyFormatter.format(costMax) + "</li>";
-    }
-    if(treatmentCost < 1 && treatmentCost !== ""){
-        errorList += "<li>Treatment costs must be at least $1.</li>";
-    }
+    //Validate treatment and assessment costs if the payment type is fee split
+    if($('#id_paymentType').find(":selected").text() === 'Fee Split'){
+        //Treatment Cost
+        if(treatmentCost === ""){
+            errorList += "<li>Please enter a Treatment Cost.</li>";
+        }
+        if(treatmentCost > costMax && treatmentCost !== ""){
+            errorList += "<li>Treatment costs cannot exceed " + currencyFormatter.format(costMax) + "</li>";
+        }
+        if(treatmentCost < 1 && treatmentCost !== ""){
+            errorList += "<li>Treatment costs must be at least $1.</li>";
+        }
 
-    //Minimum # of Treatments
-    if(treatmentMin === ""){
-        errorList += "<li>Please enter a Minimum Number of Treatments.</li>";
-    }
-    if(treatmentMin > sessionMax && treatmentMin !== ""){
-        errorList += "<li>Minimum number of Treatment sessions cannot exceed " + sessionMax + "</li>";
-    }
-    if(treatmentMin < 1 && treatmentMin !== ""){
-        errorList += "<li>Minimum number of Treatment sessions cannot be negative.</li>";
-    }
+        //Minimum # of Treatments
+        if(treatmentMin === ""){
+            errorList += "<li>Please enter a Minimum Number of Treatments.</li>";
+        }
+        if(treatmentMin > sessionMax && treatmentMin !== ""){
+            errorList += "<li>Minimum number of Treatment sessions cannot exceed " + sessionMax + "</li>";
+        }
+        if(treatmentMin < 1 && treatmentMin !== ""){
+            errorList += "<li>Minimum number of Treatment sessions cannot be negative.</li>";
+        }
 
-    //Assessment Cost
-    if(assessmentCost === ""){
-        errorList += "<li>Please enter a Assessment Cost.</li>";
-    }
-    if(assessmentCost > costMax && assessmentCost !== ""){
-        errorList += "<li>Assessment costs cannot exceed " + currencyFormatter.format(costMax) + "</li>";
-    }
-    if(assessmentCost < 1 && assessmentCost !== ""){
-        errorList += "<li>Assessment costs must be at least $1.</li>";
-    }
+        //Assessment Cost
+        if(assessmentCost === ""){
+            errorList += "<li>Please enter a Assessment Cost.</li>";
+        }
+        if(assessmentCost > costMax && assessmentCost !== ""){
+            errorList += "<li>Assessment costs cannot exceed " + currencyFormatter.format(costMax) + "</li>";
+        }
+        if(assessmentCost < 1 && assessmentCost !== ""){
+            errorList += "<li>Assessment costs must be at least $1.</li>";
+        }
 
-    //Minimum # of Assessments
-    if(assessmentMin === ""){
-        errorList += "<li>Please enter a Minimum Number of Assessments.</li>";
+        //Minimum # of Assessments
+        if(assessmentMin === ""){
+            errorList += "<li>Please enter a Minimum Number of Assessments.</li>";
+        }
+        if(assessmentMin > sessionMax && assessmentMin !== ""){
+            errorList += "<li>Minimum Number of Assessments cannot exceed " + sessionMax + "</li>";
+        }
+        if(assessmentMin < 1 && assessmentMin !== ""){
+            errorList += "<li>Minimum number of Assessments sessions cannot be negative.</li>";
+        }
     }
-    if(assessmentMin > sessionMax && assessmentMin !== ""){
-        errorList += "<li>Minimum Number of Assessments cannot exceed " + sessionMax + "</li>";
-    }
-    if(assessmentMin < 1 && assessmentMin !== ""){
-        errorList += "<li>Minimum number of Assessments sessions cannot be negative.</li>";
-    }
+    
 
     //Validate Therapist Scheudle
     let mondayVal = check_times(mondayStart, mondayEnd, 'Monday');
