3cc0f17f334114f02d73e5e2cc99b2dd2efcc9e2
Luke Oribine <lukeoribine@Lukes-MacBook-Air.local>
2023-10-15 07:34:01 -0400

    Updates to T&C

--- auction/views.py
+++ auction/views.py
@@ -92,6 +92,11 @@ def view_all_auctions(request):
     auctions = Auction.objects.filter(active=True).order_by('-auctionEnd') | Auction.objects.filter(closed=True).order_by('-auctionEnd')
     return render(request, 'auction/partials/auction_list.html', {'auction': auctions})
 
+def auction_search(request):
+    auctions = ''
+    auctions = Auction.objects.filter(active=True).order_by('-auctionEnd') | Auction.objects.filter(closed=True).order_by('-auctionEnd')
+    return render(request, 'auction/partials/auction_list.html', {'auction': auctions})
+
 def terms_and_conditions(request):
     return render(request, 'auction/terms_and_conditions.html', {})
 
