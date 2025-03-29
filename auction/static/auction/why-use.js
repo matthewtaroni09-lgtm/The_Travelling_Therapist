ce9aeeeb8358e8922037d9e42677cf4ed3016847
traveling-therapist <85437350+luke-oribine@users.noreply.github.com>
2025-03-29 10:13:42 -0400

    Adding read more/less animations.

--- auction/static/auction/why-use.js
+++ auction/static/auction/why-use.js
@@ -1,9 +1,37 @@
 $(document).ready(function () {
-    $("#readMoreOneButton").click(function () {
-        $("#readMoreOneModal").modal('show');
+    var showChar = 600;
+    var ellipsestext = "...";
+
+    $(".truncate").each(function () {
+        var content = $(this).html();
+        if (content.length > showChar) {
+            var c = content.substr(0, showChar);
+            var h = content;
+            var html =
+                '<div class="truncate-text" style="display:block">' +
+                c +
+                '<span class="moreellipses">' +
+                ellipsestext +
+                '&nbsp;&nbsp;<br><button href="" class="btn btn-primary moreless more">Read More</button></span></span></div><div class="truncate-text" style="display:none">' +
+                h +
+                '<button href="" class="btn btn-primary moreless less">Read Less</button></span></div>';
+
+            $(this).html(html);
+        }
     });
 
-    $("#readMoreTwoButton").click(function () {
-        $("#readMoreTwoModal").modal('show');
+    $(".moreless").click(function () {
+        var thisEl = $(this);
+        var cT = thisEl.closest(".truncate-text");
+        var tX = ".truncate-text";
+
+        if (thisEl.hasClass("less")) {
+            cT.prev(tX).toggle();
+            cT.slideToggle();
+        } else {
+            cT.toggle();
+            cT.next(tX).fadeToggle();
+        }
+        return false;
     });
 });
