318235f9e303b19cda6668396108b42d6aedf1b7
traveling-therapist <info@travelingtherapist.ca>
2025-11-27 12:17:41 -0500

    Adding clinic hiring guide to Nav.

--- auction/urls.py
+++ auction/urls.py
@@ -34,7 +34,6 @@ urlpatterns = [
     path("how_it_works", views.how_it_works, name="how-it-works"),
     path("mission_vision", views.mission_vision, name="mission-vision"),
     path("clinic_owner_guide", views.clinic_owner_guide, name="clinic-owner-guide"),
-    path("clinic_owner_guide_2", views.clinic_owner_guide_2, name="clinic-owner-guide-2"),
 
     # AJAX Calls
     path('auction/data/auction/<auction_id>', views.get_auction_end, name="get-auction-end"),
--- auction/views.py
+++ auction/views.py
@@ -217,9 +217,6 @@ def cookie_policy(request):
 def clinic_owner_guide(request):
     return render(request, 'auction/clinic_owner_guide.html', {})
 
-def clinic_owner_guide_2(request):
-    return render(request, 'auction/clinic_owner_guide_2.html', {})
-
 def profile(request):
     if request.user.is_authenticated == False:
         return render(request, 'auction/profile.html', {})
