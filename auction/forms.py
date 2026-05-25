50c24166d8974faa243e6975c91472130a7d74f7
fontana4ca <fontana4ca@gmail.com>
2026-05-25 15:00:06 -0400

    bugfix

--- auction/forms.py
+++ auction/forms.py
@@ -65,7 +65,7 @@ def validate_file_extension(value, image_name):
         if not ext.lower() in valid_extensions:
             error_list.append(ValidationError(u'Unsupported file extension for ' + str(image_name) + '. Valid file types are' + ', '.join(valid_extensions) + '.'))
         
-        if value.size > 10242880:
+        if value.size > 10485760:
             error_list.append(ValidationError(u'Max file size exceeded for ' + str(image_name) + ', images must be less than 10 MB.'))
 
     return error_list
